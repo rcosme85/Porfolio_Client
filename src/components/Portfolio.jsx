29bd90b318582e413cac4a6b1054ecf2072cc842
“rcosme85” <rcosme85@hotmail.com>
2023-12-07 22:15:48 -0500

    Update portfolio - description

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,7 +11,7 @@ export const Portfolio = () => {
             {/* Primer Proyecto */}
             <div className={styles.proyecto}>
               <img
-                src="assets/img/portfolio/thumbnails/1-Dogs-650x350.jpg"
+                src="assets/img/portfolio/ToDo-F.jpg"
                 alt
               />
               <div className={styles.overlay}>
@@ -30,7 +30,7 @@ export const Portfolio = () => {
             {/* Segundo Proyecto */}
             <div className={styles.proyecto}>
               <img
-                src="assets/img/portfolio/thumbnails/1-Dogs-650x350.jpg"
+                src="assets/img/portfolio/HotelOasis-2.jpg"
                 alt
               />
               <div className={styles.overlay}>
@@ -42,8 +42,48 @@ export const Portfolio = () => {
                 <p className="fw-light fs-6">
                   Hotel web application, the objective of this application is to
                   search for available rooms in a period of time to make an
-                  online reservation. Technologies: React, Redux,
-                  Css, Tailwind, JavaScript, NodeJs, Express and PostgreSQL
+                  online reservation. Technologies: React, Redux, Css, Tailwind,
+                  JavaScript, NodeJs, Express and PostgreSQL
+                </p>
+              </div>
+            </div>
+            {/* Tercer Proyecto */}
+            <div className={styles.proyecto}>
+              <img
+                src="assets/img/portfolio/SoulMusic-2.png"
+                alt
+              />
+              <div className={styles.overlay}>
+                <h3>Link - Soul Music</h3>
+                {/* <p>Fotografía</p> */}
+              </div>
+              <div>
+                <h4 className="text-center text-success">Soul Music</h4>
+                <p className="fw-light fs-6">
+                  Music and vinyl sales web application. You can register to use
+                  the service to choose and create your own playList of your
+                  favorite music. The following technologies were used: React,
+                  CSS, Bootstrap, Node, Express and PostgreSQL
+                </p>
+              </div>
+            </div>
+            {/* Cuarto Proyecto */}
+            <div className={styles.proyecto}>
+              <img
+                src="assets/img/portfolio/1-Dogs-650x350.jpg"
+                alt
+              />
+              <div className={styles.overlay}>
+                <h3>Link - Dogs</h3>
+                {/* <p>Fotografía</p> */}
+              </div>
+              <div>
+                <h4 className="text-center text-success">Dogs</h4>
+                <p className="fw-light fs-6">
+                  Construction of a Single Page Application from the API
+                  (TheDogApi) in which you can: Search, filter, order and create
+                  new dogs. The technologies of: React, Redux, CSS, Node,
+                  JavaScript, Express, PostgreSQL and Sequelize were used.
                 </p>
               </div>
             </div>
